4130cf420d740b16ae68b60a22fc564e66445aa1
Jeanie Crossfield Zatkulak <jeaniezatkulak@gmail.com>
2024-10-02 11:43:14 -0700

    jcz squashing bugs

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,93 +1,118 @@
-<div class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
-    <x-application-logo class="block h-12 w-auto" />
-
-    <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
-        Welcome to your LPD
-    </h1>
-
-    <p class="mt-6 text-gray-500 dark:text-gray-400 leading-relaxed">
-       Jeanie Testing
-    </p>
-</div>
-
-<div class="bg-gray-200 dark:bg-gray-800 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
-    <div>
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
-            </svg>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
-                <a href="https://laravel.com/docs">Documentation</a>
+<template>
+    <app-layout>
+        <template #header>
+            <h2
+                class="font-semibold text-xl text-gray-800 leading-tight float-left"
+            >
+                Dashboard
             </h2>
+            <div
+                class="hidden w-full md:block md:w-auto align-middle"
+                id="navbar-default"
+            >
+                <ul
+                    class="font-medium flex float-right flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+                >
+                    <li>
+                        <a
+                            href="dashboard"
+                            class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                            aria-current="page"
+                        >Process List</a
+                        >
+                    </li>
+                    <li>
+                        <a
+                            href="search"
+                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                        >Search</a
+                        >
+                    </li>
+                    <li>
+                        <a
+                            href="account-balance"
+                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                        >Account Balance</a
+                        >
+                    </li>
+                    <li>
+                        <a
+                            href="mailto:%63on%74ac%74@lp%73nv%2E%63om"
+                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                        >Email Us</a
+                        >
+                    </li>
+                </ul>
+            </div>
+        </template>
+
+        <div class="basis-3/4">
+            <div class="py-2">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-4">
+                    <div class="my-5"></div>
+                    <outstanding-firm
+                        :customers="outFirm"
+                        :sixmoago="sixmoago"
+                    />
+                </div>
+            </div>
+
+            <div class="py-2">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-4">
+                    <div class="my-5"></div>
+                    <paid-invoices :invoices="outInvoices" />
+                </div>
+            </div>
         </div>
 
-        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-           Jeanie Testing 2
-        </p>
-
-        <p class="mt-4 text-sm">
-            <a href="https://laravel.com/docs" class="inline-flex items-center font-semibold text-indigo-700 dark:text-indigo-300">
-                Explore the documentation
-
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ms-1 w-5 h-5 fill-indigo-500 dark:fill-indigo-200">
-                    <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
-                </svg>
-            </a>
-        </p>
-    </div>
-
-    <div>
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
-            </svg>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
-                <a href="https://laracasts.com">Laracasts</a>
-            </h2>
+        <div class="basis-1/4">
+            <div class="py-2">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-4">
+                    <div class="my-5"></div>
+                    <right-nav />
+                    <right-nav2 />
+                </div>
+            </div>
         </div>
+    </app-layout>
+</template>
 
-        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-            Jeanie 2 Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
-        </p>
+<script>
+    import AppLayout from "@/Layouts/AppLayout";
+    import Welcome from "@/Jetstream/Welcome";
+    import OutstandingInvoices from "@/Pages/Dashboard/OutstandingInvoices";
+    import RightNav from "@/Pages/Dashboard/RightNav";
+    import RightNav2 from "@/Pages/Dashboard/RightNav2";
+    import PaidInvoices from "@/Pages/Dashboard/PaidInvoices";
+    import AccountBalance from "@/Pages/Dashboard/AccountBalance";
+    import {useRemember} from "@inertiajs/vue3";
+    import OutstandingFirm from "@/Pages/Dashboard/OutstandingFirm.vue";
 
-        <p class="mt-4 text-sm">
-            <a href="https://laracasts.com" class="inline-flex items-center font-semibold text-indigo-700 dark:text-indigo-300">
-                Start watching Laracasts
-
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ms-1 w-5 h-5 fill-indigo-500 dark:fill-indigo-200">
-                    <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
-                </svg>
-            </a>
-        </p>
-    </div>
-
-    <div>
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-            </svg>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
-                <a href="https://tailwindcss.com/">Tailwind</a>
-            </h2>
-        </div>
-
-        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-            Laravel Jetstream is built with Tailwind, an amazing utility first CSS framework that doesn't get in your way. You'll be amazed how easily you can build and maintain fresh, modern designs with this wonderful framework at your fingertips.
-        </p>
-    </div>
-
-    <div>
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-            </svg>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
-                Authentication
-            </h2>
-        </div>
-
-        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-            Authentication and registration views are included with Laravel Jetstream, as well as support for user email verification and resetting forgotten passwords. So, you're free to get started with what matters most: building your application.
-        </p>
-    </div>
-</div>
+    export default {
+        name: "Dashboard",
+        components: {
+            PaidInvoices,
+            AccountBalance,
+            OutstandingInvoices,
+            OutstandingFirm,
+            RightNav,
+            RightNav2,
+            AppLayout,
+            Welcome,
+        },
+        props: {
+            outInvoices: Object,
+            outFirm: Object,
+            customers: Object,
+            sixmoago: String,
+            customer: Object,
+        },
+        remember: { data: ["tab"], key: "Dashboard" },
+        data() {
+            return {
+                tab: 1,
+            };
+        },
+        methods: {},
+    };
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,62 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ClientResponseController;
+use App\Http\Controllers\AttemptController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\AuthClientController;
+use App\Http\Controllers\FirmController;
+use App\Http\Controllers\InvoiceController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::get('/test',function(){
-   return \App\Models\User::all();
-});
+//Route::get('/', function () {
+//    return Inertia::render('Welcome', [
+//        'canLogin' => Route::has('login'),
+//        'canRegister' => Route::has('register'),
+//        'laravelVersion' => Application::VERSION,
+//        'phpVersion' => PHP_VERSION,
+//    ]);
+//});
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-})->middleware('auth')->name('welcome');
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+    return redirect()->route('dashboard');
 });
+
+
+Route::middleware(['auth:sanctum', 'verified', 'client.linked'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'show'])->name('dashboard');
+    Route::get('/firm/{id}', [FirmController::class, 'get'])->name('firm-detail');
+    Route::get('/invoice/{id}', [InvoiceController::class, 'get'])->name('invoice.detail');
+    Route::get('/attempt/{id}', [AttemptController::class, 'show'])->name('attempt.detail');
+    Route::get('/attempt', [AttemptController::class, 'show'])->name('attempt');
+    Route::get('/search', [SearchController::class, 'show'])->name('search');
+    Route::get('/client-response/{id}', [ClientResponseController::class, 'show'])->name('client-response');
+    Route::get('/firm', [FirmController::class, 'show'])->name('firm');
+    Route::get('/account-balance', [FirmController::class, 'show'])->name('account-balance');
+    Route::get('/dashboard/paid-invoices', [DashboardController::class, 'getPaidInvoices'])->name('dashboard-paid-invoices');
+    Route::get('/invoice/{id}/pay', [InvoiceController::class, 'pay'])->name('invoice.pay');
+    Route::get('/authclient', [AuthClientController::class, 'show'])->name('auth-client');
+    Route::post('/invoice/{id}/pay', [InvoiceController::class, 'submitPayment'])->name('invoice.pay.submit');
+    Route::post('/invoice/{id}/stripe/getClientSecret', [InvoiceController::class, 'getStripeClientSecret'])->name('invoice.pay.stripe.get-client-secret');
+    Route::get('/get_customer', [DashboardController::class, 'getCustomer'])->name('get-customer');
+    Route::post('/client-email', [ClientResponseController::class, 'store'])->name('client-response.store');
+});
+
+Route::middleware(['web', 'verified', 'auth:sanctum'])->group(function(){
+    Route::get('/user/account-link', [AccountController::class, 'showLinkAccount'])->name('user.link-account');
+    Route::post('/user/account-link', [AccountController::class, 'storeLinkAccount'])->name('user.link-account.store');
+});
+
+
