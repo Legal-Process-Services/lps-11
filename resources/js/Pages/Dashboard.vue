6af0261492959c73ccb4ab02eef2ec35b3c72585
Jeanie Crossfield Zatkulak <jeaniezatkulak@gmail.com>
2025-05-22 14:07:19 -0700

    remove div end in dashboard.vue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -14,7 +14,7 @@
                     :customers="outFirm"
                     :sixmoago="sixmoago"
                 />
-            </div>
+
 
             <div class="py-2">
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-4">
